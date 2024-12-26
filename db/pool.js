4584776e54f23987a4ff234dca4483cda1a2c30a
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-12-26 15:23:54 +0530

    ssl required

--- db/pool.js
+++ db/pool.js
@@ -6,7 +6,10 @@ const pool = new Pool({
     user: process.env.DATABASE_USER,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT
+    port: process.env.DATABASE_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports =  pool 
